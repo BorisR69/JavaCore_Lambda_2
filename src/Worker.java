f40b2e98ceb311c7bb0e4b7f8b4be45b5163fd37
BorisR69 <97660890+BorisR69@users.noreply.github.com>
2022-05-30 22:03:25 +0300

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -11,7 +11,7 @@ public class Worker {
         for (int i = 0; i < 100; i++) {
             if (i == 33) {
                 errorCallback.onError("Task " + i + " is error");
-            }
+            } else
             callback.onDone("Task " + i + " is done");
         }
     }
